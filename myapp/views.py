fe3979c182ec87b2153ee2f1dd9a069e546e663d
Jesus Peve Andazabal <jesusmauriciopeveandazabal@gmail.com>
2023-06-19 12:02:36 -0500

    Mejorar al miniProyecto - uso de templates y url

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from .models import Project, Task
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateNewTask, CreateNewProject
 
 
 # Create your views here.
 def index(request):
-   title = 'Django Course!!'
+   title = 'Django MiniProyecto Prueba'
    return render(request,'index.html', {
       'title' : title
    })
@@ -25,13 +26,31 @@ def hello(request, username):
 def projects(request):
    #projects = list(Project.objects.values())
    projects = Project.objects.all()
-   return render(request, 'projects.html',{
+   return render(request, 'projects/projects.html',{
       'projects' : projects
    })
 
 def tasks(request):
    #task = get_list_or_404(Task, id=id)
    tasks = Task.objects.all()
-   return render(request, 'tasks.html',{
+   return render(request, 'tasks/tasks.html', {
       'tasks' : tasks
    })
+
+def create_task(request):
+   if request.method == 'GET':
+      return render(request, 'tasks/create_task.html',{
+         'form' : CreateNewTask()
+   }) 
+   else:
+      Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=2)
+      return redirect('/tasks/')
+   
+def create_project(request):
+      if request.method == 'GET':   
+         return render(request, 'projects/create_project.html',{
+         'form' : CreateNewProject()
+      })
+      else:
+          Project.objects.create(name=request.POST["name"])
+          redirect('projects')
